9fd25f286e90d1273f43d4291d19fd3dc30f773d
Benya9669 <mrtrololoshka222@gmail.com>
2020-11-12 11:35:39 +0300

    Fix mistakes

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -70,12 +70,10 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String {
-    return when {
-        age % 10 in 2..4 && age % 100 !in 12..14 -> "$age года"
-        age % 10 == 1 && age % 100 != 11 -> "$age год"
-        else -> "$age лет"
-    }
+fun ageDescription(age: Int): String = when {
+    age % 10 in 2..4 && age % 100 !in 12..14 -> "$age года"
+    age % 10 == 1 && age % 100 != 11 -> "$age год"
+    else -> "$age лет"
 }
 
 /**
@@ -105,10 +103,12 @@ fun whichRookThreatens(
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
 ): Int {
+    val underAttack1 = rookX1 == kingX || rookY1 == kingY
+    val underAttack2 = rookX2 == kingX || rookY2 == kingY
     return when {
-        rookX1 != kingX && rookX2 != kingX && rookY1 != kingY && rookY2 != kingY -> 0
-        (rookX1 == kingX || rookY1 == kingY) && (rookX2 != kingX && rookY2 != kingY) -> 1
-        (rookX2 == kingX || rookY2 == kingY) && (rookX1 != kingX && rookY1 != kingY) -> 2
+        !underAttack1 && !underAttack2 -> 0
+        underAttack1 && !underAttack2 -> 1
+        !underAttack1 && underAttack2 -> 2
         else -> 3
     }
 }
